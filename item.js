38a05be0dee0712c102ddb98df4a10d8bd7b01ec
cristianriera <7criera@stpatrick.org>
2016-12-12 10:51:24 -0600

    Finished Item

---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,s){
+  this.name = n;
+  this.price = p;
+  this.shipping = s;
+}
+
+
+}
+
+//Create your three test items below this comment.
+let mygift = new Item("PS4",400,7);
+let yourgift = new Item("Ipad",300,10);
+let agift = new Item("Ball",60,5);
