623851561b31c793ef43637f0fcdb88d5ea59d8c
cristianriera <7criera@stpatrick.org>
2016-12-13 10:45:22 -0600

    Add files via upload

--- item.js
+++ item.js
@@ -13,3 +13,22 @@ constructor(n,p,s){
 let mygift = new Item("PS4",400,7);
 let yourgift = new Item("Ipad",300,10);
 let agift = new Item("Ball",60,5);
+
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor(i,q){
+  this.itemList(i);
+  this.itemQuantity(q);
+  }
+  addItem(i,q){
+
+  }
+
+  }
+  totalCart(){
+    let total = 0;
+    for(i=0; i<this.itemList.length i++){
+      total +=(this.itemList[i].price*this,itemQuantity[i]);
+    }
+      return total;
+    }
